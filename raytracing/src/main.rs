0c9769c20f8a799296433ff64f683bd52ab88934
nathanhleung <github@nathanhleung.com>
2022-08-24 10:30:16 -0700

    Add Raytracing and latest SICP

--- raytracing/src/main.rs
+++ raytracing/src/main.rs
@@ -6,6 +6,7 @@ fn main() {
 	println!("{} {}", image_width, image_height);
 	println!("255");
 	for i in 0..image_width {
+		eprintln!("Scanlines remaining: {}", image_width - i);
 		for j in 0..image_height {
 			let r = i as f64 / image_width as f64;
 			let g = j as f64 / image_height as f64;
@@ -18,4 +19,5 @@ fn main() {
 			println!("{} {} {}", ir, ig, ib);
 		}	
 	}	
+	eprintln!("Done.");
 }
